be90133876dd5ed64b1eac7e7cbf08567b679ae2
CRShayan <cr4rishi@gmail.com>
2026-04-24 21:09:12 +0530

    Add application startup and API configuration for the Smart Campus service

---
+++ src/main/java/com/smartcampus/api/Main.java
@@ -0,0 +1,25 @@
+package com.smartcampus.api;
+
+import java.io.IOException;
+import java.net.URI;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Main {
+
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig config = new ResourceConfig().packages("com.smartcampus.api");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("Smart Campus API is running at " + BASE_URI);
+        System.out.println("Press Enter to stop the server...");
+        System.in.read();
+        server.shutdownNow();
+    }
+}
